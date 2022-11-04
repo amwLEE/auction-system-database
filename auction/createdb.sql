5ca2a58938bbf6d5fa9bc48a2ca1d137b62f0021
Amanda Mei Wah Lee <amanda.lee19@imperial.ac.uk>
2022-11-04 21:03:23 +0000

    added additional tables to database

--- auction/createdb.sql
+++ auction/createdb.sql
@@ -23,5 +23,37 @@ CREATE TABLE Users
 )
 ENGINE = InnoDB;
 
-INSERT INTO Users (firstName, lastName, email, username, password)
-    VALUES ('Amanda', 'Lee', 'amanda.lee.22@ucl.ac.uk', 'amwLEE', '123');
+CREATE TABLE Category
+(
+    categoryID INT AUTO_INCREMENT PRIMARY KEY,
+    categoryName VARCHAR(64) NOT NULL
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Auction
+(
+    itemID INT AUTO_INCREMENT PRIMARY KEY,
+    itemName VARCHAR(64) NOT NULL,
+    itemDescription VARCHAR(255) NOT NULL,
+    sellerID INT,
+    categoryID INT,
+    startDateTime TIMESTAMP NOT NULL,
+    endDateTime TIMESTAMP NOT NULL,
+    startingPrice DECIMAL(12,2) NOT NULL,
+    reservePrice DECIMAL(12,2) NOT NULL,
+    FOREIGN KEY (sellerID) REFERENCES Users(userID),
+    FOREIGN KEY (categoryID) REFERENCES category(categoryID)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Bid
+(
+    bidID INT AUTO_INCREMENT PRIMARY KEY,
+    itemID INT,
+    buyerID INT,
+    timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    bidPrice DECIMAL(12,2) NOT NULL,
+    FOREIGN KEY (itemID) REFERENCES Auction(itemID),
+    FOREIGN KEY (buyerID) REFERENCES Users(userID)
+)
+ENGINE = InnoDB;
