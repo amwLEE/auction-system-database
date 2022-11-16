8f7e8521b621bb37262726606c9233e2d009181a
RubiksCubey <dylankc.gaming@gmail.com>
2022-11-16 19:34:27 +0000

    created simple bid form and started process_bid

---
+++ auction/bid_form.php
@@ -0,0 +1,52 @@
+<?php
+    include_once 'header.php';
+    include_once 'database.php'; 
+    require("utilities.php");
+?>
+
+<div class="container">
+    <html>
+    <head>
+        <title>Bid Creation Form</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body>
+    <h2 class="my-3">Bid Creation Form</h2>
+        <!-- Create Bid Registration Form -->
+        <form action="bid_form.php" method="POST"> 
+            <div class= "message">
+                <?php
+                include_once 'process_bid.php';
+                    global $error_msg;
+
+                    if (isset($success) && ($success == true) ){
+                    echo '<p style="color:green;">Your bid has been submitted successfully!<p>';
+                    }else{
+                    echo '<p style="color:red;">'.$error_msg.'</p>'; //display error message
+                    }
+                ?>
+            </div>
+            
+            <!-- All Parameters: bidID, itemID, buyerID, bidTimeStamp, bidPrice -->
+            <div class="form-group row">
+                <!-- Bid Price -->
+                <label for="bidPrice" class="col-sm-2 col-form-label text-right">Bid Price ($):</label>
+	                <div class="col-sm-3">
+                        <input type="number" class="form-control" id="bidPrice" name="bidPrice" placeholder="0.00" min="0" step="0.01">
+                        <!-- Should we set the minimum value to the current bid price or 0? -->
+                        <small id="bidPriceHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	                </div>
+            
+            <div class="form-group">
+                <button type="submit" name="submitBidForm" value="submitBidForm" class="btn btn-success form-control">Submit Bid</button>
+            </div>
+
+            </div>
+        </form>
+    </body>
+</div>            
+
+<?php include_once("footer.php")?>
+
+
