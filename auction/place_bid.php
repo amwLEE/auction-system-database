9a647300aff478b50606049a104a4034584866d5
RubiksCubey <ucabdkc@ucl.ac.uk>
2022-12-09 20:56:40 +0000

    Implemented sendmail feature

--- auction/place_bid.php
+++ auction/place_bid.php
@@ -2,6 +2,7 @@
 // TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
 // Notify user of success/failure and redirect/give navigation options.
 include 'database.php';
+include 'sendEmail.php';
    
     if (isset($_POST['submitBidForm'])){
         // Add data to Bid table in AuctionDB database
@@ -22,6 +23,7 @@ include 'database.php';
             $query = mysqli_query($connection, "SELECT * FROM bid WHERE buyerID=$buyerID AND bidTimeStamp=NOW()");
             if (mysqli_num_rows($query) == 1 ){
                 $success = true;
+                emailWatcher($itemID, $buyerID, $bidPrice);
             } else {
                 $error_msg = 'An error occurred and your bid was not submitted';
             }
